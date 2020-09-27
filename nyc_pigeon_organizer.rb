7acd92b00b5ac58c89d395635146c90113de9ed0
Christopher Demahy <demahyc@gmail.com>
2020-09-27 22:44:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,7 +11,7 @@ def nyc_pigeon_organizer(data)
     value.collect do |key, value|
       # Now we are at the inner arrays of names for each category and description
       # Push all the names to an array so we can sort them
-      value.each { |item| names_array.push(item) }
+      value.each { |item| names_array.push(item.to_sym) }
       # Get rid of any duplicate names in the array
       names_array.uniq!
 
