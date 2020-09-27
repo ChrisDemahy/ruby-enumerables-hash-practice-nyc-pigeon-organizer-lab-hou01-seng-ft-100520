bed6e5039ca9bc66e6aa4acee6488f60b0d89817
Christopher Demahy <demahyc@gmail.com>
2020-09-27 23:07:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -17,8 +17,24 @@ def nyc_pigeon_organizer(data)
 
     end
   end
+  # Convert the names array into a hash
  names_array.each { |item| new_data[item]= Hash.new }
 
+ # Reassemble the rest of the data into the new name hash
+ data.collect do |key,value|
+   # Store the category (top level)
+   category = key
+   value.collect do |key, value|
+     # Store the desctiption(second level) grey, male, etc
+     description = key
 
+     # Use each name to assign each the category/description pair
+     value.each do |item|
+       new_data[item][category] = description
+     end  
+     
+   end
+ end
+ 
   binding.pry
 end
