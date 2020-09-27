f14db30e9387f550e4bf01ff1752a2f2d42871a2
Christopher Demahy <demahyc@gmail.com>
2020-09-27 21:58:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
   new_data = Hash.new
   names_array = Array.new
   # Collect the names of the pidgeions into an array
-  data.collect do |key,value| 
+  data.collect do |key,value|
     value.collect do |key, value|
       # Now we are at the inner arrays of names for each category and description
       # Push all the names to an array so we can sort them
