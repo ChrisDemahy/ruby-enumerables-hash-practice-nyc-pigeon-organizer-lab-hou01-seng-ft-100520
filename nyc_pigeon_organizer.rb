a5a5ce2e6eded60c668b8318028aff1727b96721
Christopher Demahy <demahyc@gmail.com>
2020-09-27 21:52:22 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  # Convert hash data provided into a different structured hash 
+  # Organizes pidgeons to be by name instead of by attribute
+  new_data = Hash.new
+  names_array = Array.new
+  # Collect the names of the pidgeions into an array
+  data.collect do |key,value| 
+
+    value.collect do |key, value| 
+      value.each |item| do
+        names_array.push(item)
+      end
+    end
+  end
+  
 end
