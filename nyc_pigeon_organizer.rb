e271c109560461924ceae1f621c9080c414c024a
Christopher Demahy <demahyc@gmail.com>
2020-09-27 23:12:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -30,7 +30,6 @@ def nyc_pigeon_organizer(data)
 
      # Use each name to assign each the category/description pair
      value.each do |item|
-       binding.pry
        new_data[item.to_sym].store(category, description)
      end
 
