c81823fa5297120c21530ab58bd3f7f21c1e176f
Christopher Demahy <demahyc@gmail.com>
2020-09-27 22:43:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,18 +2,16 @@ require "pry"
 def nyc_pigeon_organizer(data)
   # Convert hash data provided into a different structured hash
   # Organizes pidgeons to be by name instead of by attribute
-  new_data = []
+  new_data = Hash.new
   names_array = Array.new
   category = ""
   descrption = ""
   # Collect the names of the pidgeions into an array
   data.collect do |key,value|
-    category = key
     value.collect do |key, value|
-      description = key
       # Now we are at the inner arrays of names for each category and description
       # Push all the names to an array so we can sort them
-      value.each { |item| new_data[item][category] = description }
+      value.each { |item| names_array.push(item) }
       # Get rid of any duplicate names in the array
       names_array.uniq!
 
